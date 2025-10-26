0be012a9b2322e9c602f516c9d518f50f19222a2
Anmol Gupta <anmolgupta520@gmail.com>
2025-10-26 23:33:26 +0530

    change cell character

--- main.go
+++ main.go
@@ -14,13 +14,9 @@ import (
 
 	TODO
 	--------
-	- We need a Data Structure to store the current live cells -> we can use a Map with "(i,j)" string as the key
-	- we will need to read through this map -> apply rules -> get the new state -> print it on the terminal
 	- We will use the Raw mode and create a rectangle with borders -> (N-2)x(M-2)
 	- At any point this rectangle will show a part of the infinite 2D Grid => [A, B, C, D]
-	- At the start we will have the (0,0) on the center
 	- We can move across the grid with arrow keys
-	- We can allow the selection of seed using mouse at the start (?)
 	--------
 */
 
@@ -87,7 +83,7 @@ func (universe Universe) draw() {
 		row := offsetY - cell.Y
 		col := offsetX + cell.X
 		if row > 0 && col > 0 && row < universe.rows && col < universe.cols {
-			draw(row, col, "◻")
+			draw(row, col, "■")
 		}
 	}
 	moveToHome()
