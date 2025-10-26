d4a58daf01ee87e41282d396dfc3183e3609211c
Anmol Gupta <anmolgupta520@gmail.com>
2025-10-26 17:15:04 +0530

    add Universe struct + draw

--- main.go
+++ main.go
@@ -25,12 +25,52 @@ import (
 
 // ASCII Sequence Code - https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 
+// Grid
+type Cell struct {
+	X, Y int
+}
+type Grid map[Cell]bool
+
+func (grid Grid) addCell(i int, j int) {
+	grid[Cell{i, j}] = true
+}
+
+func (grid Grid) removeCell(i int, j int) {
+	delete(grid, Cell{i, j})
+}
+
+type Universe struct {
+	grid   Grid
+	window [4]int
+	rows   int
+	cols   int
+}
+
+func (universe Universe) draw() {
+	for cell, _ := range universe.grid {
+		draw(cell.X, cell.Y, "0")
+	}
+}
+
+// Raw mode helpers
+func draw(row int, col int, element string) {
+	fmt.Printf("\x1B[%d;%dH%s", row, col, element)
+}
+
 func clearScreen() {
 	fmt.Print("\x1B[2J")
 }
 
 func moveToHome() {
 	fmt.Print("\x1B[H")
+}
+
+func showCussor() {
+	fmt.Print("\x1b[?25l")
+}
+
+func hideCussor() {
+	fmt.Print("\x1b[?25l")
 }
 
 func main() {
@@ -42,12 +82,27 @@ func main() {
 		fmt.Println("Error enabling raw mode:", err)
 		panic(err)
 	}
+	cols, rows, err := term.GetSize(fd)
+	if err != nil {
+		panic(err)
+	}
+	hideCussor()
 	clearScreen()
 	moveToHome()
+
+	universe := Universe{
+		grid:   make(Grid),
+		window: [4]int{-rows / 2, -cols / 2, rows / 2, cols / 2},
+		rows:   rows,
+		cols:   cols,
+	}
+	universe.grid.addCell(0, 0)
 
 	// Render Loop
 	buf := make([]byte, 3)
 	for {
+		universe.draw()
+
 		_, err := os.Stdin.Read(buf)
 		if err != nil {
 			fmt.Println("Error Reading input")
@@ -66,6 +121,7 @@ func main() {
 	// Diabling Raw Mode
 	clearScreen()
 	moveToHome()
+	showCussor()
 	defer term.Restore(fd, oldState)
 
 }
